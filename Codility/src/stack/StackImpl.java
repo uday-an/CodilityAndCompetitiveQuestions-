4100066cc45e9352d162b39378e3d3db28661a02
uday-an <k.singhu6@gmail.com>
2021-08-10 01:43:24 +0530

    Stack

---
+++ Codility/src/stack/StackImpl.java
@@ -0,0 +1,22 @@
+package stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StackImpl {
+
+	
+	
+	public static void main(String[] args) {
+		int x[]=new int[5];
+		x[0]=4;
+		System.out.println(x.length);
+
+		List<List<Integer>> result=new ArrayList<List<Integer>>();
+		List<Integer> y=new ArrayList<Integer>();
+		result.add(y);
+		
+		System.out.println("hikt".compareTo("hot"));
+	}
+
+}
